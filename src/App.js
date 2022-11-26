edb9af7f9532c8f059483a8dee1d1e5d2cbf60f9
Prabhat147 <prabhat.ap21@gmail.com>
2022-11-26 20:35:12 +0545

    array is made to save the data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import Todolist from './Todolist';
+import React, { useState } from 'react';
 
-function App() {
-return (
-  <>    
-  <Todolist/>
-  </>
-  )
-}
+const App = () => {
+    const [inputList, setInputList]= useState("");
+    const [Items, setItems] = useState([]);
+
+    const itemEvent = (event) => {
+      setInputList(event.target.value);
+    };
+
+    const listOfItems = () => {
+        setItems((oldItems) => {
+          return [...oldItems, inputList];
+        });
+    };
+
+    return (
+      <>
+        <div className='main'>
+            <div className='center'>
+                <header>
+                    <h2 style={{color:"red"}}>Todo list</h2>
+                </header>
+                <div>
+                    <input type="text" id='newTask' placeholder='Enter your task here' onChange={itemEvent}></input>&nbsp;
+                    <button onClick={listOfItems}>+</button>
+                    <ol>
+                      {Items.map((itemval) => {
+                        return <li> {itemval} </li>
+                      })}
+                        
+                    </ol>
+                </div>
+            </div>
+
+        </div>
+      </>
+    );
+ };
 
 export default App;
