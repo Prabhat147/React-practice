95ed6c881a7391eab9c661be065c452325db03ce
Prabhat147 <prabhat.ap21@gmail.com>
2022-11-27 09:34:16 +0545

    css added and app is functioning to add task

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
 import React, { useState } from 'react';
+import './App.css';
+function App() {
+  const [inputList, setInputList] = useState("");
+  const [Items, setItems] = useState([]);
 
-const App = () => {
-    const [inputList, setInputList]= useState("");
-    const [Items, setItems] = useState([]);
+  const itemEvent = (event) => {
+    setInputList(event.target.value);
+  };
 
-    const itemEvent = (event) => {
-      setInputList(event.target.value);
-    };
+  const listOfItems = () => {
+    setItems((oldItems) => {
+      return [...oldItems, inputList];
+    });
+  };
 
-    const listOfItems = () => {
-        setItems((oldItems) => {
-          return [...oldItems, inputList];
-        });
-    };
+  return (
+    <>
+      <div className='main'>
+        <div className='center'>
+          <header>
+            <h1 style={{ color: "red" }}>Todo list</h1>
+          </header>
+          <div>
+            <input type="text" id='newTask' placeholder='Enter your task here' onChange={itemEvent}></input>&nbsp;
+            <button onClick={listOfItems}>+</button>
+            <ul>
+              {Items.map((itemval) => {
+                return <li> {itemval} </li>;
+              })}
 
-    return (
-      <>
-        <div className='main'>
-            <div className='center'>
-                <header>
-                    <h2 style={{color:"red"}}>Todo list</h2>
-                </header>
-                <div>
-                    <input type="text" id='newTask' placeholder='Enter your task here' onChange={itemEvent}></input>&nbsp;
-                    <button onClick={listOfItems}>+</button>
-                    <ol>
-                      {Items.map((itemval) => {
-                        return <li> {itemval} </li>
-                      })}
-                        
-                    </ol>
-                </div>
-            </div>
-
+            </ul>
+          </div>
         </div>
-      </>
-    );
- };
+
+      </div>
+    </>
+  );
+}
 
 export default App;
